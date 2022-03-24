50599ec14be2f1a226708027e8d11c8e8def61ef
djinodissingar <dissingardjino@icloud.com>
2022-03-24 10:32:55 +0100

    Création de la configuration

--- lib/detail.dart
+++ lib/detail.dart
@@ -145,7 +145,7 @@ class detailState extends State<detail>{
                   onPressed: (){
 
                     setState(() {
-                      print("message");
+
 
                       lecture == statutLecture.paused;
                     });
